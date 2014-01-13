e169fb1db9bc09df0811352a3618465130dd153d
suezhen <sz3001@gmail.com>
2014-01-13 14:57:00 +0800

    ajax head

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,17 +2,15 @@ require 'grape-swagger'
 require 'category_api'
 require 'album_api'
   class API < Grape::API
-    prefix "api"
-    
-    mount BizmassMedia::CategoryAPI
-    mount BizmassMedia::AlbumAPI
+    prefix "api"  
 
     before do
       header['Access-Control-Allow-Origin'] = '*'
       header['Access-Control-Request-Method'] = '*'
     end
 
-   
+    mount BizmassMedia::CategoryAPI
+    mount BizmassMedia::AlbumAPI
 
     add_swagger_documentation :markdown => true
     
